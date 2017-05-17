4419d2e4bc68c7969536c13529f9281d1ad4bccf
Zach <zach@192.168.1.99>
2017-05-17 01:20:14 +0100

    fixed test failure

--- src/main/java/pageobjects/SearchResultsPage.java
+++ src/main/java/pageobjects/SearchResultsPage.java
@@ -1,10 +1,12 @@
 package pageobjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import support.BrowserUtils;
 import support.Driver;
 
@@ -18,18 +20,22 @@ import java.util.Set;
  */
 public class SearchResultsPage extends Driver {
 
-
-    @FindBy(how= How.ID,using = "noResultsTitle")
+    String a = "";
+    @FindBy(how = How.ID, using = "noResultsTitle")
     public static WebElement noSearchResult;
 
-    @FindBy(how=How.CSS,using = "ul#s-results-list-atf li h2")
+    @FindBy(how = How.CSS, using = "ul#s-results-list-atf li h2")
     public static List<WebElement> resultsList;
 
-    @FindBy(how=How.CSS,using = "#s-result-count span a")
+    @FindBy(how = How.CSS, using = "#s-result-count .a-text-normal")
     public static List<WebElement> filters;
 
-    @FindBy(how= How.ID,using = "sort")
+    @FindBy(how = How.ID, using = "sort")
     public static WebElement sortBy;
+
+    @FindBy(how = How.CSS, using = "#leftNavContainer ul:first-of-type a")
+    public static List<WebElement> categoryFilters;
+
 
     private Set<String> titleLists = new HashSet<>();
 
@@ -37,24 +43,21 @@ public class SearchResultsPage extends Driver {
     public SearchResultsPage() {
         super(aDriver);
 
-        PageFactory.initElements(Driver.aDriver,this);
+        PageFactory.initElements(Driver.aDriver, this);
     }
 
-    public boolean addTitle(final String newString)
-    {
+    public boolean addTitle(final String newString) {
         return this.titleLists.add(newString);
     }
 
-    public Set<String> getTitles()
-    {
+    public Set<String> getTitles() {
         return Collections.unmodifiableSet(this.titleLists);
     }
 
 
+    public Set<String> getResultTitles() {
 
-    public Set<String> getResultTitles(){
-
-        for(WebElement result: resultsList){
+        for (WebElement result : resultsList) {
 
 
             addTitle(result.getAttribute("textContent").toString());
@@ -65,29 +68,41 @@ public class SearchResultsPage extends Driver {
 
     }
 
-    public boolean zeroSearchResults(){
+    public boolean zeroSearchResults() {
 
         return noSearchResult.isDisplayed();
     }
 
-    public String searchErrorMessage(){
+    public String searchErrorMessage() {
 
         return noSearchResult.getAttribute("textContent");
     }
 
-    public SearchResultsPage filterByCategory(String category){
+    public SearchResultsPage filterByCategory(String category) {
 
-        aDriver.findElement(By.linkText(category)).click();
 
+        try {
+
+            for (WebElement aCategory : categoryFilters) {
+
+                if (aCategory.getText().contains(category)) {
+
+                    aCategory.click();
+                }
+            }
+        } catch (StaleElementReferenceException e) {
+
+
+        }
         return this;
 
     }
 
-    public boolean resultFilteredBy(String filter){
+    public boolean resultFilteredBy(String filter) {
 
-        for(WebElement aFilter: filters){
+        for (WebElement aFilter : filters) {
 
-            if(aFilter.getText().contains(filter)){
+            if (aFilter.getText().contains(filter)) {
 
                 return true;
             }
@@ -97,12 +112,12 @@ public class SearchResultsPage extends Driver {
 
     }
 
-    public void sortBy(String option){
+    public void sortBy(String option) {
 
-       BrowserUtils.selectListOption(sortBy,option);
+        BrowserUtils.selectListOption(sortBy, option);
     }
 
-    public String sortedBy(){
+    public String sortedBy() {
 
 
         return BrowserUtils.selectedOption(sortBy).toString();
